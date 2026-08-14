fff47a52c53fc0ab574abdbbcbdb0b8d7fafffd8
Mohammed-shihaf <shihaf@Testable.Cloud>
2026-08-15 02:21:20 +0530

    microfrontend_BE_nodejs18_FE_reactjs22_FE_angularjs17: roll out frontend-react/quality/ tool fixtures and devDependencies for readiness scanner

---
+++ frontend-react/.stryker-tmp/sandbox-teYpGv/src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+// @ts-nocheck
+import PropTypes from "prop-types";
+
+export default function SearchBar({ value, onChange }) {
+  return (
+    <input
+      type="search"
+      placeholder="Search widgets…"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+}
+
+SearchBar.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
